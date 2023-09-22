bab537ac8e149c63bdff7f833dc08813a4d96b92
Igor Markin <markin.io210@gmail.com>
2023-09-22 16:16:15 +0100

    test: add mainnet forking to hardhat network (#4)

--- README.md
+++ README.md
@@ -10,4 +10,8 @@
 - `$ npx hardhat deploy --network <network>`
 
 ## Tests
+
+### Unit/integration tests
 - `$ npx hardhat test`
+
+### E2E tests
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const {
+  INFURA_API_KEY,
+} = process.env;
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
@@ -12,6 +19,15 @@ const config: HardhatUserConfig = {
   solidity: "0.8.19",
   paths: {
     deploy: 'scripts',
+  },
+  networks: {
+    hardhat: {
+      // TODO: disable forking for unit and integration tests?
+      forking: {
+        url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+        blockNumber: 18192031
+      }
+    }
   }
 };
 
