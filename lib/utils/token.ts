fda41ccc267f7bf6ca9899819c9944eb0aa14118
markin.io <markin.io210@gmail.com>
2023-11-17 09:44:30 +0000

    feat: add goerli tokens

--- lib/utils/token.ts
+++ lib/utils/token.ts
@@ -1,5 +1,5 @@
 import { HardhatNetworkConfig, Network } from "hardhat/types";
-import { MAINNET_ADDRESSES } from "../constants/tokens";
+import { MAINNET_ADDRESSES, GOERLI_ADDRESSES } from "../constants/tokens";
 
 export const getTokenListForNetwork = (network: Network) => {
   // TODO: revisit this
@@ -10,9 +10,11 @@ export const getTokenListForNetwork = (network: Network) => {
     return MAINNET_ADDRESSES;
   }
 
-  switch (network.config.chainId) {
-    case 1:
+  switch (network.name) {
+    case "mainnet":
       return MAINNET_ADDRESSES;
+    case "goerli":
+      return GOERLI_ADDRESSES;
     default:
       throw new Error(`Unsupported network: ${network.config.chainId}`);
   }
